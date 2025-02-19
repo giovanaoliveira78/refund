86d40636d43db13154dfc0beaba59d53ada6ad3f
giovana.lopes <gigioliveirafju@gmail.com>
2025-02-18 21:02:55 -0300

    obtem os dados do formulario

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 // Seleciona os elementos do formulário
-const amount =  document.getElementById("amount")
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
   //Obtém o valor atual do input e remove os caracteres não numéricos
-   let value = amount.value.replace(/\D/g, "") // Aqui recebo o valor do input, tiro as letras com /\D/g e substituo por nada, aspas vazias "" 
-   
+  let value = amount.value.replace(/\D/g, "") // Aqui recebo o valor do input, tiro as letras com /\D/g e substituo por nada, aspas vazias "" 
+
   // Transforma o valor em centavos (exemplo: 150/100 = 1.50)
-   value = Number(value) / 100
+  value = Number(value) / 100
 
 
-   // Atualiza o valor do input com o valor formatado
-   amount.value = formatCurrencyBRL(value) // aqui eu pego o novo valor de value (já formato) e atribuo ao próprio value
+  // Atualiza o valor do input com o valor formatado
+  amount.value = formatCurrencyBRL(value) // aqui eu pego o novo valor de value (já formato) e atribuo ao próprio value
 }
 
 function formatCurrencyBRL(value) {
   // Formata o valor no padrão BRL (real brasileiro)
   value = value.toLocaleString("pt-BR", {
     style: "currency",
-    currency: "BRL",  
+    currency: "BRL",
   })
 
   // Retorna o valor formatado
-    return value
+  return value
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+}       
