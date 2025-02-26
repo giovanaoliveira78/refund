0bc2ec72500ea92169542e19ea523ddb0b6d5921
giovana.lopes <gigioliveirafju@gmail.com>
2025-02-26 13:47:17 -0300

    percorre os itens da lista

--- scripts.js
+++ scripts.js
@@ -113,6 +113,15 @@ function updateTotals() {
 
     // Atualiza a quantidade de itens da lista
     expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+  
+    // Variavel para incrementar o total
+    let total = 0
+
+    //Percorre cada item (li) da lista (ul)
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount").textContent
+      
+    }
   } catch (error) {
     console.log(error)
     alert("Não foi possível atualizar os totais.")
